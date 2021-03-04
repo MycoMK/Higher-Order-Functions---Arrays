374c2648de5d263997f7006e638ab4630872533c
MICHAEL KINGS <kmichael381@gmail.com>
2021-03-04 01:52:06 -0800

    Functions-arrays

---
+++ script.js
@@ -0,0 +1,51 @@
+console.log('connected')
+
+const companies = [
+{name:'company One', category:'Finance', start:1981, end: 2003},
+{name:'company two', category:'Retail', start:1992, end: 2008},
+{name:'company three', category:'Auto', start:1999, end: 2007},
+{name:'company four', category:'Retail', start:1989, end: 2010},
+{name:'company five', category:'Technology', start:2009, end: 2014},
+{name:'company six', category:'Finance', start:1987, end: 2010},
+{name:'company seven', category:'Auto', start:1986, end: 1996},
+{name:'company eigth', category:'Technology', start:2011, end: 2016},
+{name:'company nine', category:'Retail', start:1981, end: 1989},
+
+];
+
+const age = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15,45, 25, 64, 32];
+
+// forloop
+
+for (let i = 0; i < companies.length; i++) {
+    console.log(companies[i]);
+    
+}
+
+// forEach
+
+companies.forEach(function(company){
+    console.log(company.name)
+});
+
+
+// filter
+// get 21 and older
+const canDrink = age.filter(function(age){
+if(age >= 21){
+    return true;
+}
+});
+console.log(canDrink);
+
+// filter Retail companies
+ const retailCompanies = companies.filter(function(company){
+   if(companies.category === 'Retail'){
+       return true;
+   }
+ });
+ console.log(retailCompanies);
+
+// map
+// sort
+// reduce
